c0d65aaa97935081d6d98072c6d9ce535f2cb267
MassimoAurelio <massimoaureliowork@gmail.com>
2024-02-05 17:04:54 +0300

    Первый коммит

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./authRouter')
+const cors = require('cors');
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(cors());
+
+app.use(express.json())
+app.use("/auth", authRouter)
+
+const start = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://massimo:lJYMF6DDMjKuGHMF@cluster0.kz3qhm4.mongodb.net/?retryWrites=true&w=majority`)
+
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
